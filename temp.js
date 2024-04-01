44b1d076c5860540a9f85e071f72833e0c6364d4
Rename me <rename-me@gmail.com>
2024-04-01 17:43:05 +0300

    feat(COM-2001): fixed lint errors

---
+++ temp.js
@@ -0,0 +1,10 @@
+//# Base resume and tailored
+title: "naming",
+
+//# Extend template
+{
+  id: "66e9f969-285f-4d69-876a-8169d0c15e3d",
+  name: "Template",
+  created_at: "2019-12-06T12:00:00Z",
+  updated_at: "2019-12-06T12:00:00Z",
+}
